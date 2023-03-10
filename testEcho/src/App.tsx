0b99990a8bec226f252cb584aeac08cd950a8f56
KirichenkoYV <hause99@gmail.com>
2023-03-10 19:55:02 +0300

    createRegistrationPage: done.

--- testEcho/src/App.tsx
+++ testEcho/src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Authorization from "./pages/authorizationPage/Authorization";
 import CabinetUser from "./pages/cabinetUserPage/CabinetUser";
+import RegisterPage from "./pages/registerPage/RegisterPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/auth" element={<Authorization />}></Route>
+      <Route path="/" element={<Authorization />}></Route>
+      <Route path="/register" element={<RegisterPage />}></Route>
       <Route path="/user" element={<CabinetUser />}></Route>
+     {/* <Route path="/restorePassword" element={}></Route> */}
     </Routes>
   );
 }
